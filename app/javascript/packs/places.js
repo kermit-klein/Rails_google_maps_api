f9a32b34c98f30b165d9c1a07c7feb0d5bbd0553
allezz <aerbay@gmail.com>
2020-07-22 02:55:17 +0200

    Fix webpack problem, Add places.js , NOTE WINDOW

---
+++ app/javascript/packs/places.js
@@ -0,0 +1,10 @@
+window.initMap = function (lat, lng) {
+  var myCoords = new google.maps.LatLng(lat, lng);
+  var mapOptions = {
+    center: myCoords,
+    zoom: 14,
+  };
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+};
+
+console.log("ADDED");
