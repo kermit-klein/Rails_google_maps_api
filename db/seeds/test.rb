0069e775d6b04080e50a0ca05aff5cc937c8d038
allezz <aerbay@gmail.com>
2020-07-27 15:36:32 +0200

    Change seed places

--- db/seeds/test.rb
+++ db/seeds/test.rb
@@ -1,5 +1,5 @@
 Place.create!([
-                { "name": 'Buckingham Palace', "latitude": '51.501564', "longitude": '-0.141944' },
-                { "name": 'Westminster Abbey', "latitude": '51.499581', "longitude": '-0.127309' },
-                { "name": 'Big Ben', "latitude": '51.500792', "longitude": '-0.124613' }
+                { "name": 'Mount Rushmore', "latitude": '43.87910249999999', "longitude": '-103.4590667' },
+                { "name": 'Hiroshima', "latitude": '34.3852894', "longitude": '132.4553055' },
+                { "name": 'Dubai', "latitude": '25.2048493', "longitude": '55.2707828' }
               ])
